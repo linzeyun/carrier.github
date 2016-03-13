3c9d049e1b5af1f899976fcf08d16fc72cf0080c
linzeyun <208432325@qq.com>
2016-03-13 15:14:44 +0800

    Create firstC++.cpp

---
+++ firstC++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << "a = " a << ", b = " << b << endl;
+  cout << "my first simple c++ code." << endl;
+  return 0;
+}
+  
